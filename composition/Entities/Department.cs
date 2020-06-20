6e89453dd5459ce62f12b31aae118f9961541bc6
victordcsilva <vctrslv23@gmail.com>
2020-06-20 13:31:05 -0300

    commit test

---
+++ composition/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace composition.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name )
+        {
+            Name = name;
+
+        }
+    }
+}
---
+++ composition/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace composition.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
